738ed6b129b017e0a1cec18cdadbc2b2d9b7f05e
SFMack <sfcamack@icloud.com>
2020-02-19 22:04:51 -0800

    react app created. dependencies added. beginning stages of the form

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
 
-function Form(props) {
+const Form = props => {
 	return <h2>Top Dawg</h2>;
-}
+};
 
 export default withFormik({})(Form);
