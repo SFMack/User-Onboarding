111b03c01a08a8d9a5f47dd53fc32a41e4312af3
SFMack <sfcamack@icloud.com>
2020-02-20 04:10:21 -0800

    role dropdown added. not being passed into values.

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import * as yup from 'yup';
 import axios from 'axios';
 import { withFormik, Form, Field } from 'formik';
+import { Container } from '@material-ui/core';
 
 const RegistrationForm = ({ touched, errors, status }) => {
 	const [users, setUsers] = useState([]);
@@ -12,62 +13,77 @@ const RegistrationForm = ({ touched, errors, status }) => {
 	}, [status]);
 
 	return (
-		<div className='form'>
-			<Form>
-				<label>
-					Name:
-					<Field type='text' name='name' placeholder='Name' />
-					{/* if the name field has been touched and there are errors then render them to the screen */}
-					{touched.name && errors.name && (
-						<p className='errors'>{errors.name}</p>
-					)}
-				</label>
-				<label>
-					Email:
-					<Field type='text' name='email' placeholder='Email' />
-					{touched.email && errors.email && (
-						<p className='errors'>{errors.email}</p>
-					)}
-				</label>
-				<label>
-					Password:
-					<Field type='password' name='password' placeholder='Password' />
-					{touched.password && errors.password && (
-						<p className='errors'>{errors.password}</p>
-					)}
-				</label>
-				<label>
-					Verify Password:
-					<Field
-						type='password'
-						name='verifyPassword'
-						placeholder='Verify password'
-					/>
-					{touched.verifyPassword && errors.verifyPassword && (
-						<p className='errors'>{errors.verifyPassword}</p>
-					)}
-				</label>
-				<label>
-					Terms of Service:
-					<Field type='checkbox' name='termsOfService' />
-					<span className='checkmark' />
-					{touched.termsOfService && errors.termsOfService && (
-						<p className='errors'>{errors.termsOfService}</p>
-					)}
-				</label>
+		<Container>
+			<div className='form'>
+				<Form>
+					<label>
+						Name
+						<Field type='text' name='name' placeholder='Name' />
+						{/* if the name field has been touched and there are errors then render them to the screen */}
+						{touched.name && errors.name && (
+							<p className='errors'>{errors.name}</p>
+						)}
+					</label>
+					<label>
+						Email
+						<Field type='text' name='email' placeholder='Email' />
+						{touched.email && errors.email && (
+							<p className='errors'>{errors.email}</p>
+						)}
+					</label>
+					<label>
+						Password
+						<Field type='password' name='password' placeholder='Password' />
+						{touched.password && errors.password && (
+							<p className='errors'>{errors.password}</p>
+						)}
+					</label>
+					<label>
+						Verify Password
+						<Field
+							type='password'
+							name='verifyPassword'
+							placeholder='Verify password'
+						/>
+						{touched.verifyPassword && errors.verifyPassword && (
+							<p className='errors'>{errors.verifyPassword}</p>
+						)}
+					</label>
 
-				<button type='submit'>Submit</button>
-			</Form>
-			<div>
-				{/* {users && (
+					<label>
+						Role
+						<Field as='select' name='role'>
+							<option value='choose' disabled>
+								Choose your Role
+							</option>
+							<option value='engineer'>Software Engineer</option>
+							<option value='designer'>UX Designer</option>
+							<option value='backend'>Backend Developer</option>
+						</Field>
+					</label>
+
+					<label>
+						Agree to Terms of Service
+						<Field type='checkbox' name='termsOfService' />
+						<span className='checkmark' />
+						{touched.termsOfService && errors.termsOfService && (
+							<p className='errors'>{errors.termsOfService}</p>
+						)}
+					</label>
+
+					<button type='submit'>Submit</button>
+				</Form>
+				<div>
+					{/* {users && (
 					<ul key={users.id}>
 						<li>Name: {users.name}</li>
 						<li>Email: {users.email}</li>
 					</ul>
 				)} */}
-				{console.log(users)}
+					{console.log(users)}
+				</div>
 			</div>
-		</div>
+		</Container>
 	);
 };
 
@@ -76,6 +92,7 @@ export default withFormik({
 		name: props.name || '',
 		email: props.email || '',
 		password: '',
+		role: '',
 		termsOfService: false
 	}),
 	validationSchema: yup.object().shape({
